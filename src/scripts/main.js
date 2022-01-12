6732d4a892e14aec2d2b89dde9a14b242e2bc343
Diki Siswanto <dickyachmady@gmail.com>
2022-01-12 08:59:33 +0800

    add navbar, slider, and post items in homepage

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
-const a = 10;
+/* eslint-disable no-undef */
+$(document).ready(() => {
+  const num = $('.owl-carousel').data('itemsnumber') || 1;
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    items: num,
+    autoplay: true,
+    dots: true,
+  });
+});
